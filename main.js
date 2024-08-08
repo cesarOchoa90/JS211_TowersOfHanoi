8f9cee4b200d6718e2a4f97881c9d687e359206f
cesarOchoa90 <hire.cesar@outlook.com>
2024-08-07 22:27:21 -0500

    logic completed, tests working

--- main.js
+++ main.js
@@ -30,27 +30,65 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
+const movePiece = (startStack , endStack) => {
   // Your code here
+  // we are going to take the last element of the array
+  // move it to another array
+ 
+ const removedPiece = stacks[startStack].pop()
+ stacks[endStack].push(removedPiece)
 
 }
 
+
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+const isLegal = (startStack , endStack) => {
   // Your code here
+
+  // use a conditional to check if the code is allowed 
+  // large numbers should not be stack on top of smaller number
+  // I believe I need to check if whatever numbers gets moved is not placed in an array that has smaller item so
+  //maybe using .length to check the array
+  //or maybe I need to check if the number moved[from the starstack] is greater than the number that is
+  //on the endStack?
+  if (!stacks[endStack].length){
+    console.log("move is legal");
+    return true
+  }
+  if (stacks[startStack][stacks[startStack].length -1] < stacks[endStack][stacks[endStack].length -1] ){
+    console.log("move is legal");
+    return true
+  } else {
+    console.log("move is not allowed")
+    return false
+  }
 
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
+
+// checks the array when all removedpieces are in order from large number first to small number using .length
+// if this is the case then is true and print out "you win"
 const checkForWin = () => {
   // Your code here
+  if( stacks.b.length == 4 || stacks.c.length == 4 ){
+    console.log("you win!")
+    return true
+  }
+  else{
+    return false
+  }
 
 }
 
 // When is this function called? What should it do with its argument?
+// this is the main code that will run the whole game , check if you win and if the moves are legal
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
-
+  if (isLegal(startStack, endStack)){
+    movePiece(startStack, endStack);
+    checkForWin();
+  }
 }
 
 const getPrompt = () => {
